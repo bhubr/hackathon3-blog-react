2eb328c4362c89d4c81c02ce46a790b307304af8
Benoît Hubert <benoithubert@gmail.com>
2021-01-13 11:56:17 +0100

    Add React Helmet to handle title&meta

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,14 @@
 import PostList from './post-list'
+import { Helmet } from 'react-helmet'
 
 function Home() {
   return (
     <main>
+      <Helmet>
+        <title>Blog React avec optimisations SEO</title>
+        <link rel="canonical" href="https://hack3-blog-react-sans-seo.netlify.app/" />
+        <meta name="description" content="Voir les bonnes pratiques SEO, notamment avec React" />
+      </Helmet>
       <h1>Blog React avec optimisations SEO</h1>
       <PostList />
     </main>
--- src/components/post-details.js
+++ src/components/post-details.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet'
 import axios from 'axios';
 
 function PostDetails() {
@@ -17,6 +18,11 @@ function PostDetails() {
   }
   return (
     <article className="PostDetails">
+      <Helmet>
+        <title>{post.title}</title>
+        <link rel="canonical" href="https://hack3-blog-react-sans-seo.netlify.app/" />
+        <meta name="description" content={post.description} />
+      </Helmet>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
       <Link to="/">Retour à l'accueil</Link>
